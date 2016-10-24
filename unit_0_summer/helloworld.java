4ff65c296ac0d0ee1e6ead10a6cbb0396d4ba54b
blasinasalazaruag <blasinasalazar@uagateway.org>
2016-10-24 18:11:04 +0000

    initial commit

---
+++ unit_0_summer/helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("hello world!");
+        System.out.println("can school start already because I hate summer!");
+    }
+}
